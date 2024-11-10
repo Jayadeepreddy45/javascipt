2adc42fba65a916f7a46c51732ffc57dc5ad3dea
Jayadeepreddy45 <Jayadeepreddy452002@gmail.com>
2024-11-10 21:24:49 +0530

    first commit

---
+++ datatypes.js
@@ -0,0 +1,18 @@
+let name = "Jayadeep"
+let age = 21
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof name); // undefined
+console.log(typeof null); // object
